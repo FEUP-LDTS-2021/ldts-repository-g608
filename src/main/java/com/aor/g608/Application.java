807c57c28333fbfeac96258027c1913c89e05809
Vinicius <vinimcorrea@hotmail.com>
2022-01-03 21:02:55 +0000

    git commit -m "added everything"

---
+++ src/main/java/com/aor/g608/Application.java
@@ -0,0 +1,7 @@
+package com.aor.g608;
+
+public class Application {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
---
+++ src/main/java/com/aor/g608/Game.java
@@ -0,0 +1,33 @@
+package com.aor.g608;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+
+public class Game {
+    public Game(){
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('O')[0]);
+            screen.refresh();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
