9faaaf1ac0a108f32ea84c2c556861a52569f1f1
Vinicius <vinimcorrea@hotmail.com>
2022-01-03 21:32:03 +0000

    added some stuff

--- src/main/java/com/aor/g608/Game.java
+++ src/main/java/com/aor/g608/Game.java
@@ -1,16 +1,24 @@
 package com.aor.g608;
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+
+
 import java.io.IOException;
 
 
 public class Game {
+    private Screen screen;
+    private int x = 10, y = 10;
+
     public Game(){
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -22,12 +30,38 @@ public class Game {
             screen.startScreen();
             screen.doResizeIfNecessary();
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('O')[0]);
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('O')[0]);
             screen.refresh();
         } catch (IOException e){
             e.printStackTrace();
         }
 
+    }
+
+    public void run() throws IOException {
+        try {
+            while(true) {
+                draw();
+                KeyStroke key = screen.readInput();
+                processKey(key);
+
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == ('q'))
+                    screen.close();
+                if (key.getKeyType() == KeyType.EOF)
+                    break;
+
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void draw() throws IOException{
+
+    }
+
+    private void processKey(KeyStroke key){
+        System.out.println(key);
     }
 
 }
